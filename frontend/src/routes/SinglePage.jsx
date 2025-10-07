52c92fc068a79f4db57c6ef04f47ad1e1b0460d6
Harshita <harshitaprasad.13@gmail.com>
2025-10-07 12:36:31 +0530

    slider added

--- frontend/src/routes/SinglePage.jsx
+++ frontend/src/routes/SinglePage.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import Slider from "../components/Slider";
+import "./SinglePage.scss";
+import { singlePostData, userData } from "../lib/dummydata";
+import Pin from "../assets/pin.png";
 const SinglePage = () => {
   return (
-    <div>
-      singlePage
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src={Pin} alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">{singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SinglePage
+export default SinglePage;
